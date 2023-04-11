7ee0a193a8aa8dfd271aafc140dfff227a84a060
Paulo Sanchez <pauloszt@gmail.com>
2023-04-11 17:29:10 -0500

    feat: Implement U1 and U2 Rockets Classes

---
+++ src/main/kotlin/models/U2.kt
@@ -0,0 +1,19 @@
+package models
+
+class U2 : Rocket(
+    maxWeight = 29,
+    cost = 100,
+    weight = 10,
+    chanceToExplodeOnLaunch = 0.05,
+    chanceToExplodeOnLand = 0.01
+) {
+    override fun launch(): Boolean {
+        val randomNumber: Float = (0..100).random().toFloat()
+        return randomNumber > ((chanceToExplodeOnLaunch * (currentCarryTotal / maxLoad)) * 100)
+    }
+
+    override fun landing(): Boolean {
+        val randomNumber: Float = (0..100).random().toFloat()
+        return randomNumber > ((chanceToExplodeOnLand * (currentCarryTotal / maxLoad)) * 100)
+    }
+}
