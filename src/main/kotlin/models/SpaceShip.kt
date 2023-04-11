d7a347ca62d5bf8cfff658f4b34067384c8f766f
Paulo Sanchez <pauloszt@gmail.com>
2023-04-11 17:23:16 -0500

    feat: Implement Rocket Class

--- src/main/kotlin/models/SpaceShip.kt
+++ src/main/kotlin/models/SpaceShip.kt
@@ -3,6 +3,6 @@ package models
 interface SpaceShip {
     fun launch(): Boolean
     fun landing(): Boolean
-    fun canCarry(item:Item): Boolean
-    fun carry(item:Item)
+    fun canCarry(item: Item): Boolean
+    fun carry(item: Item)
 }
